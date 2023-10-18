a34221b3f11900d16cd856d27322fd96d6037385
kautsar <kautsarrifqi1@gmail.com>
2023-10-18 18:41:20 +0700

    first commit

---
+++ routes/users.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+const userController = require('../controller/users')
+
+router.get('/users', userController.index)
+
+router.post('/user', userController.store)
+
+router.put('/user/:id', userController.update)
+
+router.delete('/user/:id', userController.delete)
+
+module.exports = router;
